7a07fc5fa4530a16a657cccbc636399de1443ee9
[dyad] <davi.andrade.viana2022@gmail.com>
2025-11-19 01:37:16 -0300

    [dyad] Implemented simple router in main.tsx to fix payment status page redirection. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,25 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import PagamentoConfirmado from "./PagamentoConfirmado.tsx";
+import PagamentoPendente from "./PagamentoPendente.tsx";
+import PagamentoFalhou from "./PagamentoFalhou.tsx";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+function Router() {
+  const path = window.location.pathname;
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  if (path.includes("/pagamento-confirmado")) {
+    return <PagamentoConfirmado />;
+  }
+  if (path.includes("/pagamento-pendente")) {
+    return <PagamentoPendente />;
+  }
+  if (path.includes("/pagamento-falhou")) {
+    return <PagamentoFalhou />;
+  }
   
+  // Rota padrão: Landing Page
+  return <App />;
+}
+
+createRoot(document.getElementById("root")!).render(<Router />);
